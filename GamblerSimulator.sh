c4df1112fc63af216a7b4310c661789a095b09a9
PalakPartani <partanipalak16@gmail.com>
2020-01-28 11:10:35 +0530

    ADDED:function to calculate win and loss total

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -7,8 +7,15 @@ calcPercent()
 }
 
 checkBet() {
+	#for 20 days
 	calcPercent
+	#if taken inside for loop then it win go upto amount 50 only
+	win=0
+	loss=0
+	for ((i=1;i<=20;i++))
+	do
 	cash=$STAKE
+		
 	while (($cash>$minVal && $cash<$maxVal))
 	do
 		if (($((RANDOM%2))==1))
@@ -18,7 +25,16 @@ checkBet() {
 			cash=$(($cash-$BET))
 		fi
 	done
-echo "Cash for the day : " $cash
+		if (( $cash > 100 ))
+		then
+		 	win=$(( $win + 50 ))
+		else	
+       		        loss=$(( $loss + 50 )) 	
+	fi	
+
+done
+echo "Total winning amount : $win"
+echo "Total losing amount :  $loss "
 }
 
 echo "Welcome to Gambler Simulation !"
