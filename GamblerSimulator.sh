0036b8f8898f83fa567c432b89175b9a3930334a
PalakPartani <partanipalak16@gmail.com>
2020-01-28 10:37:36 +0530

    ADDED:function for simulating one day cash

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-checkBet() {
-	if (($((RANDOM%2))==1))
-		then
-			echo "Win!"
-		else
-			echo "Lose!"
-		fi
+calcPercent()
+{
+	percent=$(($STAKE*50/100))
+	minVal=$(($STAKE-$percent))
+	maxVal=$(($STAKE+$percent))
+}
 
+checkBet() {
+	calcPercent
+	cash=$STAKE
+	while (($cash>$minVal && $cash<$maxVal))
+	do
+		if (($((RANDOM%2))==1))
+		then
+			cash=$(($cash+$BET))
+		else
+			cash=$(($cash-$BET))
+		fi
+	done
+echo "Cash for the day : " $cash
 }
 
 echo "Welcome to Gambler Simulation !"
