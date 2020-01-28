179ab15d13e284336927bf845243598d3b3c2f08
PalakPartani <partanipalak16@gmail.com>
2020-01-28 10:19:24 +0530

    ADDED:function to check win or lose

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-echo "Welcome to Gambler Simulation !"
+checkBet() {
+	if (($((RANDOM%2))==1))
+		then
+			echo "Win!"
+		else
+			echo "Lose!"
+		fi
 
+}
+
+echo "Welcome to Gambler Simulation !"
 #Declaring constants
 readonly STAKE=100
 readonly BET=1
-
-
+checkBet
 
