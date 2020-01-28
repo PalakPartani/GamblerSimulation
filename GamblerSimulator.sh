b7dc55fbd90d8ea9d52fb62520d820d5373cc657
PalakPartani <partanipalak16@gmail.com>
2020-01-28 19:02:34 +0530

    ADDED:while loop to repeat if last value greater than 0

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-calcPercent()
+calculatePercent()
 {
 	percent=$(($STAKE*50/100))
 	minVal=$(($STAKE-$percent))
@@ -8,14 +8,11 @@ calcPercent()
 
 checkBet() {
 	#for 20 days
-	calcPercent
+	calculatePercent
 	#if taken inside for loop then it will go upto amount 50 only
-	win=0
-	loss=0
 	for ((i=1;i<=20;i++))
 	do
-	cash=$STAKE
-		
+		cash=$STAKE	
 	while (($cash>$minVal && $cash<$maxVal))
 	do
 		if (($((RANDOM%2))==1))
@@ -29,10 +26,11 @@ checkBet() {
 gamblerDict[$i]=$(($cash-$STAKE))
 done
 }
+
 #Adding daily amount that is win or loss 
-addVal()
-{
-i=2
+addValues(){
+
+	i=2
 	while ((i<=20))
 	do
 		gamblerDict[$i]=$((${gamblerDict[$i]}+${gamblerDict[$((i-1))]}))
@@ -44,23 +42,34 @@ i=2
 
 #checking the day where amount is greatest or lowest
 checkLuck() {
-addVal
-echo "Luckiest day : "
-for day in ${!gamblerDict[@]}
-do
-      echo $day ${gamblerDict[$day]}
-done | sort -k2 -rn | head -n 1
+	addValues
+	echo "Luckiest day : "
+	for day in ${!gamblerDict[@]}
+	do
+    		echo $day ${gamblerDict[$day]}
+	done | sort -k2 -rn | head -n 1
 
-echo "unluckiest day"
-for day in ${!gamblerDict[@]}
-do
-      echo $day ${gamblerDict[$day]}
-done | sort -k2 -n | head -n 1
+	echo "unluckiest day :"
+	for day in ${!gamblerDict[@]}
+	do
+     		echo $day ${gamblerDict[$day]}
+	done | sort -k2 -n | head -n 1
 
 }
+checkWinRepeat()
+{
+	while((${gamblerDict[20]}>0))
+	do
+		checkBet
+		checkLuck
+	done
+	echo "Sorry!Can't Play!"
+}
+		
 echo "Welcome to Gambler Simulation !"
 #Declaring constants
 readonly STAKE=100
 readonly BET=1
 checkBet
 checkLuck
+checkWinRepeat
